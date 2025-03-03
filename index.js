8a427bc1b76b384dec9b3420ce76cfde971cf7d1
developer-sultan <sultans.blogs@gmail.com>
2025-03-03 12:52:20 +0600

    added keyboard navigation features

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const accordion = document.getElementsByClassName("faq-card_summary");
+const accordion = document.querySelectorAll(".faq-card_summary");
+
+// Hide/Show answer when question is clicked
+for(let i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener(("click"), faqToggle);
+}
+
+// Hide/Show answer using keyboard navigation
 
 for(let i = 0; i < accordion.length; i++){
-    accordion[i].addEventListener(("click"), function(){
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if(panel.style.maxHeight){
-            panel.style.maxHeight = null;
-        }else{
-            panel.style.maxHeight = panel.scrollHeight + "px";
+    accordion[i].addEventListener("keydown",function(event){
+        if(event.key ===  "Enter" || event.key === " " || event.key === "Tab"){
+            faqToggle.call(this);
         }
     });
+}
+
+// Hide/Show answer main function
+function faqToggle(){
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if(panel.style.maxHeight){
+        panel.style.maxHeight = null;
+    }else{
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
 }
